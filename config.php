586df0ac6eb087e38900c620e99bea98ca3f46db
Ronald Bautista <ronbau21@hotmail.com>
2021-12-19 21:08:40 -0400

    Ultimo commit (se modifico el archivo .gitignore)

--- config.php
+++ config.php
@@ -7,6 +7,6 @@ define('DEFAULT_PAGE','pages/inicio.php');
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASSWORD','');
-define('DB_NAME','bd_proyectoblog');
+define('DB_NAME','db_proyecto');
 define('DB_PORT','3306');
 ?>
